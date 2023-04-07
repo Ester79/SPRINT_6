ad628636ab6b0f2987984a5ba4625d0557830882
Ester79 <esterramoscuervo@hotmail.com>
2023-04-07 21:41:52 +0200

    Level 1: Ex1

---
+++ sprint6/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Escena from './components/escena/Escena';
+
+function App() {
+  return (
+    <>
+      <Escena></Escena>
+
+    </>
+  );
+}
+
+export default App;
